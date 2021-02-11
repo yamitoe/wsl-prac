2990b99d8f9ba95612722886446162fba2a2d9e6
Snow <>
2021-02-11 11:48:48 -0800

    Lab 5 Ajax -  validate, errormsg, done

--- school/public/COMP351/labs/4/editFile.js
+++ school/public/COMP351/labs/4/editFile.js
@@ -3,24 +3,25 @@ const url = require('url');
 const fs = require('fs');
 http.createServer(function (req, res) {
     const q = url.parse(req.url, true);
-// if(q.pathname == "/COMP351/labs/4/writeFile/"){
+if(q.pathname == "/COMP351/labs/4/writeFile/"){
     
     fs.appendFile('./file.txt', q.query.text + "\r\n", function (err) {
         if (err) throw err;
         console.log('Saved!');
       });
     return res.end();
-// }
-// else{
-//     fs.readFile('./file.txt',function (err, data) {
-//         if (err) {
-//             res.writeHead(404, { 'Content-Type': 'text/html' });
-//             return res.end("404 Not Found");
-//         }
-//         res.writeHead(200, { 'Content-Type': 'text/html' });
-//         res.write(data);
-//         return res.end();
-//     });   
-// }
+}
+else if(q.pathname == "/COMP351/labs/4/readFile/file.txt")
+{
+    fs.readFile('./file.txt',function (err, data) {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            return res.end("404 Not Found");
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
+    });   
+}
 }).listen(8080);
 console.log("listening");
